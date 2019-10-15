398d0aee29b6e3f06542f223671d81d34237a35f
Mor Codou GUEYE <morcodou@hotmail.com>
2019-10-14 22:29:29 -0400

    adding sign in form, form input and custom button

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import  Header from './components/header/header.component'
+import Header from './components/header/header.component';
+import SinInAndSingUpPage from './pages/sing-in-and-sing-up/sing-in-and-sing-up.component';
 
 import './App.css';
 
 function App() {
   return (
     <div>
-    <Header />
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SinInAndSingUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './sing-in-and-sing-up.styles.scss';
+
+import SingIn from '../../components/sign-in/sign-in.component';
+
+const SinInAndSingUpPage = () => (
+    <div className='sing-in-and-sing-up'>
+        <SingIn />
+    </div>
+);
+
+export default SinInAndSingUpPage;
