3bb1005415a5ccba77210aaa559cb0fc4cb163ff
Mor Codou GUEYE <morcodou@hotmail.com>
2019-10-16 00:53:37 -0400

    implemented firebase utils, include ability to store authentificated users

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SinInAndSingUpPage from './pages/sing-in-and-sing-up/sing-in-and-sing-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -21,9 +21,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+          
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
   componentWillUnmount() {
@@ -33,7 +48,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
--- src/pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx
+++ src/pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './sing-in-and-sing-up.styles.scss';
 
 import SingIn from '../../components/sign-in/sign-in.component';
+import SingUp from '../../components/sign-up/sign-up.component';
 
 const SinInAndSingUpPage = () => (
     <div className='sing-in-and-sing-up'>
         <SingIn />
+        <SingUp />
     </div>
 );
 
