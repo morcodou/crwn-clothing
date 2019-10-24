c2463e13978003c5fb99def4d1e31fd5e900f23a
Mor Codou GUEYE <morcodou@hotmail.com>
2019-10-23 23:34:34 -0400

    add styled-components and use styled-component on home page, header and custom button components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import './directory.styles.scss';
 import { connect } from 'react-redux';
 
-import { selectDirectorySections } from './../../redux/directory/directory.selectors';
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 import { createStructuredSelector } from 'reselect';
 
-import MenuItem from './../menu-item/menu-item.component';
+import MenuItem from '../menu-item/menu-item.component';
 
 const Directory = ({ sections }) => (
   <div className='directory-menu'>
